1446a843c96457f299ce6f5c38e99fbaba00bbd8
Camilly <134735689+CamysRL@users.noreply.github.com>
2026-02-26 17:56:28 -0300

    Refactor theme toggle function for path consistency

--- js/script.js
+++ js/script.js
@@ -4,15 +4,16 @@ function trocarTema() {
     const modo = document.getElementById("icon-modo");
     const icon = document.getElementById("icon");
 
-    if (link.getAttribute("href") === "/css/style-light-mode.css") {
-        link.setAttribute("href", "/css/style-dark-mode.css");
-        logo.setAttribute("src", "/img/logo-tecboard-branco.png");
-        modo.setAttribute("src", "/img/icon-modo-claro.svg");
-        icon.setAttribute("href", "/favicon-tecboard-branco.svg");
+    if (link.getAttribute("href") === "css/style-light-mode.css") {
+        link.setAttribute("href", "css/style-dark-mode.css");
+        logo.setAttribute("src", "img/logo-tecboard-branco.png");
+        modo.setAttribute("src", "img/icon-modo-claro.svg");
+        icon.setAttribute("href", "favicon-tecboard-branco.svg");
     } else {
-        link.setAttribute("href", "/css/style-light-mode.css");
-        logo.setAttribute("src", "/img/logo-tecboard-preto.png");
-        modo.setAttribute("src", "/img/icon-modo-escuro.svg");
-        icon.setAttribute("href", "/favicon-tecboard-preto.svg");
+        link.setAttribute("href", "css/style-light-mode.css");
+        logo.setAttribute("src", "img/logo-tecboard-preto.png");
+        modo.setAttribute("src", "img/icon-modo-escuro.svg");
+        icon.setAttribute("href", "favicon-tecboard-preto.svg");
     }
+
 }
